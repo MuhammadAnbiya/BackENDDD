a090a63749d7b2dd8fb3587429c635378f191bfb
MuhammadAnbiya23 <anbiya17agustus@gmail.com>
2024-05-22 02:39:35 +0700

    benerin-logic

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 from flask import Flask, session, flash, redirect, url_for, render_template
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-from flask_mail import Mail, Message
-
 import os
 
 app = Flask(__name__)
 db = SQLAlchemy()
 api = Api(app)
-mail = Mail(app)
-
 
 filename = os.path.dirname(os.path.abspath(__file__))
 database = "sqlite:///" + os.path.join(filename, "db.sqlite")
@@ -17,18 +13,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = database
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = 'RahasiaKabupatenSukabumi'
 
-app.config["MAIL_SERVER"] = "smtp.gmail.com"
-app.config["MAIL_PORT"] = 465
-app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USERNAME"] = "muhammadanbiya284@gmail.com"
-app.config["MAIL_PASSWORD"] = "puguhanpohosandinanaon"
-app.config["MAIL_DEFAULT_SENDER"] = "muhammadanbiya284@gmail.com"
-
-
-
 db.init_app(app)
-mail.init_app(app)
-
 
 with app.app_context():
     db.create_all()
--- utils.py
+++ utils.py
@@ -1,42 +1,7 @@
 import re
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from app import app
-
-import smtplib
 
 def is_valid_email(email):
     return re.match(r"[^@]+@[^@]+\.[^@]+", email) is not None
 
 def is_valid_password(password):
     return len(password) >= 8
-
-def send_verification_email(email, captcha_code):
-    sender_email = app.config["MAIL_USERNAME"] 
-    sender_password = app.config["MAIL_PASSWORD"]      
-
-    receiver_email = email
-    msg = MIMEMultipart()
-
-    msg['From'] = sender_email
-    msg['To'] = receiver_email
-    msg['Subject'] = "Verifikasi CAPTCHA"
-
-    body = f"Kode CAPTCHA Anda adalah: {captcha_code}. Silakan masukkan kode ini untuk verifikasi."
-
-    msg.attach(MIMEText(body, 'plain'))
-
-    try:
-        server = smtplib.SMTP('smtp.gmail.com', 587)
-        server.starttls()
-        server.login(sender_email, sender_password)
-        server.sendmail(sender_email, receiver_email, msg.as_string())
-        print("Email verifikasi CAPTCHA telah berhasil dikirim.")
-    except Exception as e:
-        print(f"Terjadi kesalahan saat mengirim email: {e}")
-    finally:
-        server.quit()
-
-email = "anbiya17agustus@gmail.com"  
-captcha_code = "123456"           
-send_verification_email(email, captcha_code)
